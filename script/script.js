216e5431f48f7838c4d2c579a9042f72945b07e3
Albina <suyundikovaalbina43@gmail.com>
2024-04-11 15:02:02 +0300

    Добавлены новые файлы

---
+++ script/script.js
@@ -0,0 +1,25 @@
+
+
+let currentIndex = 0;
+const carouselItems = document.querySelectorAll('.slide');
+
+function goToSlide(index) {
+    if (index < 0) {
+        index = carouselItems.length - 1;
+    } else if (index >= carouselItems.length) {
+        index = 0;
+    }
+    currentIndex = index;
+    document.querySelector('.carousel').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function goToNextSlide() {
+    goToSlide(currentIndex + 1);
+}
+
+function goToPrevSlide() {
+    goToSlide(currentIndex - 1);
+}
+
+document.querySelector('.prev').addEventListener('click', goToPrevSlide);
+document.querySelector('.next').addEventListener('click', goToNextSlide);
