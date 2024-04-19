f950e43cd84f9a8242020719eceeebf885728142
Владислав Филатов <filatov@reg.ru>
2024-04-20 00:33:50 +0800

    Поправил пути в url картинок

--- script/script.js
+++ script/script.js
@@ -1,25 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let currentIndex = 0;
+    const carouselItems = document.querySelectorAll('.slide');
 
-
-let currentIndex = 0;
-const carouselItems = document.querySelectorAll('.slide');
-
-function goToSlide(index) {
-    if (index < 0) {
-        index = carouselItems.length - 1;
-    } else if (index >= carouselItems.length) {
-        index = 0;
+    function goToSlide(index) {
+        if (index < 0) {
+            index = carouselItems.length - 1;
+        } else if (index >= carouselItems.length) {
+            index = 0;
+        }
+        currentIndex = index;
+        document.querySelector('.carousel').style.transform = `translateX(-${currentIndex * 100}%)`;
     }
-    currentIndex = index;
-    document.querySelector('.carousel').style.transform = `translateX(-${currentIndex * 100}%)`;
-}
 
-function goToNextSlide() {
-    goToSlide(currentIndex + 1);
-}
+    function goToNextSlide() {
+        goToSlide(currentIndex + 1);
+    }
 
-function goToPrevSlide() {
-    goToSlide(currentIndex - 1);
-}
+    function goToPrevSlide() {
+        goToSlide(currentIndex - 1);
+    }
 
-document.querySelector('.prev').addEventListener('click', goToPrevSlide);
-document.querySelector('.next').addEventListener('click', goToNextSlide);
+    document.querySelector('.prev').addEventListener('click', goToPrevSlide);
+    document.querySelector('.next').addEventListener('click', goToNextSlide);
+});
