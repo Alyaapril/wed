1004cb70b8b94936fd686c92567a0091b45c7eb2
Albina <suyundikovaalbina43@gmail.com>
2024-04-19 21:29:35 +0300

    js3

--- script/script.js
+++ script/script.js
@@ -2,16 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentIndex = 0;
     let startX;
     let touchMoved = false;
+    const carousel = document.querySelector('.carousel');
+    const carouselItems = document.querySelectorAll('.slide');
+    const totalSlides = carouselItems.length;
 
     function goToSlide(index) {
-        const carouselItems = document.querySelectorAll('.slide');
-        if (index < 0) {
-            index = carouselItems.length - 1;
-        } else if (index >= carouselItems.length) {
-            index = 0;
-        }
         currentIndex = index;
-        document.querySelector('.carousel').style.transform = `translateX(-${currentIndex * 100}%)`;
+        if (currentIndex < 0) {
+            currentIndex = totalSlides - 1;
+        } else if (currentIndex >= totalSlides) {
+            currentIndex = 0;
+        }
+        carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
 
     function handleTouchStart(event) {
@@ -33,9 +35,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const xDiff = startX - event.changedTouches[0].clientX;
         if (Math.abs(xDiff) > 50) { // Check if swipe distance is significant
             if (xDiff > 0) {
-                goToNextSlide();
+                goToSlide(currentIndex + 1);
             } else {
-                goToPrevSlide();
+                goToSlide(currentIndex - 1);
             }
         }
         startX = null;
@@ -51,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector('.prev').addEventListener('click', goToPrevSlide);
     document.querySelector('.next').addEventListener('click', goToNextSlide);
-    document.querySelector('.carousel').addEventListener('touchstart', handleTouchStart);
-    document.querySelector('.carousel').addEventListener('touchmove', handleTouchMove);
-    document.querySelector('.carousel').addEventListener('touchend', handleTouchEnd);
+    carousel.addEventListener('touchstart', handleTouchStart);
+    carousel.addEventListener('touchmove', handleTouchMove);
+    carousel.addEventListener('touchend', handleTouchEnd);
 });
