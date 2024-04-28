2fb75dcbb3fac11f9fd20c89be2099108dabb878
Albina <suyundikovaalbina43@gmail.com>
2024-04-28 16:08:18 +0300

    21

--- script/script.js
+++ script/script.js
@@ -26,47 +26,47 @@
 
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     let currentIndex = 0;
-//     let startX = 0;
-//     let isDragging = false;
-//     const carouselItems = document.querySelectorAll('.carousel');
+document.addEventListener("DOMContentLoaded", () => {
+    let currentIndex = 0;
+    let startX = 0;
+    let isDragging = false;
+    const carouselItems = document.querySelectorAll('.carousel');
 
-//     function goToSlide(i) {
-//         if (i < 0) {
-//             i = carouselItems.length - 1;
-//         } else if (i >= carouselItems.length) {
-//             i = 0;
-//         }
-//         currentIndex = i;
-//         document.querySelector('.carousel').style.transform = `translateX(-${currentIndex * 100}%)`;
-//     }
+    function goToSlide(i) {
+        if (i < 0) {
+            i = carouselItems.length - 1;
+        } else if (i >= carouselItems.length) {
+            i = 0;
+        }
+        currentIndex = i;
+        document.querySelector('.carousel').style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
 
-//     function handleTouchStart(event) {
-//         isDragging = true;
-//         startX = event.touches[0].clientX;
-//     }
+    function handleTouchStart(event) {
+        isDragging = true;
+        startX = event.touches[0].clientX;
+    }
 
-//     function handleTouchMove(event) {
-//         if (!isDragging) return;
-//         const xDiff = event.touches[0].clientX - startX;
-//         if (xDiff > 0) {
-//             goToSlide(currentIndex - 1);
-//         } else {
-//             goToSlide(currentIndex + 1);
-//         }
-//         isDragging = false;
-//     }
+    function handleTouchMove(event) {
+        if (!isDragging) return;
+        const xDiff = event.touches[0].clientX - startX;
+        if (xDiff > 0) {
+            goToSlide(currentIndex - 1);
+        } else {
+            goToSlide(currentIndex + 1);
+        }
+        isDragging = false;
+    }
 
-//     function handleTouchEnd() {
-//         isDragging = false;
-//     }
+    function handleTouchEnd() {
+        isDragging = false;
+    }
 
-//     document.querySelector('.prev').addEventListener('click', () => goToSlide(currentIndex - 1));
-//     document.querySelector('.next').addEventListener('click', () => goToSlide(currentIndex + 1));
-//     document.addEventListener('touchstart', handleTouchStart);
-//     document.addEventListener('touchmove', handleTouchMove);
-//     document.addEventListener('touchend', handleTouchEnd);
-// });
+    document.querySelector('.prev').addEventListener('click', () => goToSlide(currentIndex - 1));
+    document.querySelector('.next').addEventListener('click', () => goToSlide(currentIndex + 1));
+    document.addEventListener('touchstart', handleTouchStart);
+    document.addEventListener('touchmove', handleTouchMove);
+    document.addEventListener('touchend', handleTouchEnd);
+});
 
 
